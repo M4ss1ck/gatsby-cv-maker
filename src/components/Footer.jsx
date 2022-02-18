e35e1f189a727e48598da77861e8b31a16de6ff7
M4ss1ck <4ndyraul@gmail.com>
2022-02-17 20:09:05 -0500

    add: source code link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
     <footer className="bg-gradient-to-r from-blue-800 via-black to-blue-800 dark:from-black dark:via-gray-900 dark:to-black text-gray-400 w-full text-center">
       Hecho con <span className="text-blue-700">💙</span> usando Gatsby,
       Tailwind CSS y Netlify{" "}
-      <a href="https://github.com/M4ss1ck" className="text-blue-700">
+      <a
+        href="https://github.com/M4ss1ck/gatsby-cv-maker"
+        className="text-blue-700"
+      >
         (Código fuente aquí)
       </a>
     </footer>
