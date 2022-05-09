5ad8025bb08e9ece6be05684753c74a63e83fdce
M4ss1ck <4ndyraul@gmail.com>
2022-05-09 11:36:41 -0400

    fix: type errors

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -12,21 +12,19 @@ export default function Error404() {
   return (
     <>
       <Seo lang={language} title={t("Error 404")} />
-      <main className="container mx-auto flex flex-col items-center justify-center h-screen w-full lg:text-2xl text-primario dark:text-gray-400 dark:bg-black font-montserrat">
-        <DarkToggle />
+      <main className="container flex flex-col items-center justify-center w-full h-screen mx-auto lg:text-2xl text-primario dark:text-gray-400 dark:bg-black font-montserrat">
+        <DarkToggle css="relative m-auto" />
         <Language />
-        <h1 className="font-bold text-lg lg:text-6xl my-auto">
+        <h1 className="my-auto text-lg font-bold lg:text-6xl">
           <Trans>Ups</Trans>
         </h1>
         <p className="">
-          <Trans>
-            La página solicitada no existe
-          </Trans>
+          <Trans>La página solicitada no existe</Trans>
         </p>
         <p>
           <Trans>La recomendación es ir a crear tu CV</Trans>
         </p>
-        <button className="border-2 border-blue-700 text-blue-700 font-bold p-4 hover:text-gray-300 hover:bg-blue-700 rounded-xl px-4 my-12">
+        <button className="p-4 px-4 my-12 font-bold text-blue-700 border-2 border-blue-700 hover:text-gray-300 hover:bg-blue-700 rounded-xl">
           <a href="/">
             <Trans>Crear CV</Trans>
           </a>
