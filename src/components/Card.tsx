76e9e00f9734c1c5b25339eacb402547dd714768
M4ss1ck <4ndyraul@gmail.com>
2022-05-27 00:24:07 -0400

    fix Card border color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Card: React.FC = ({ children }) => {
   return (
-    <div className="flex flex-col items-start justify-center w-3/4 px-4 m-2 border-2 rounded-lg border-primario">
+    <div className="flex flex-col items-start justify-center w-3/4 px-4 m-2 border-2 rounded-lg border-primary">
       {children}
     </div>
   )
