09d7bddc62e835e974ecd562c16080c0534d3034
M4ss1ck <4ndyraul@gmail.com>
2022-05-28 00:46:40 -0400

    fix: footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import { Trans } from "gatsby-plugin-react-i18next"
 
 const Footer = () => {
   return (
-    <footer className="w-full text-center bg-gradient-to-r from-primario via-white to-primario text-secundario">
-      <Trans>Hecho con</Trans> <span className="text-primario">💙</span>{" "}
+    <footer className="w-full text-center bg-gradient-to-r from-primary via-transparent to-primary text-secondary">
+      <Trans>Hecho con</Trans> <span aria-label="heart">💙</span>{" "}
       <Trans>usando Gatsby, Tailwind CSS y Netlify</Trans>{" "}
       <a
         href="https://github.com/M4ss1ck/gatsby-cv-maker"
-        className="text-primario"
+        className="text-accent"
       >
         <Trans>(Código fuente aquí)</Trans>
       </a>
