5bdcb65173e8a561235414c7dc7f24366030a948
M4ss1ck <4ndyraul@gmail.com>
2022-05-28 00:29:19 -0400

    fix Cards colors

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Card: React.FC = ({ children }) => {
   return (
-    <div className="flex flex-col items-start justify-center w-3/4 px-4 m-2 border-2 rounded-lg border-primary">
+    <div className="flex flex-col items-start justify-center w-3/4 px-4 m-2 border-2 rounded-lg border-primary bg-base-200">
       {children}
     </div>
   )
