5e38c177cbac7315f92fa399280219495c26a372
Massick <4ndyraul@gmail.com>
2023-01-26 00:19:37 -0500

    make footer taller

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import { Trans } from "gatsby-plugin-react-i18next"
 
 const Footer = () => {
   return (
-    <footer className="w-full text-center bg-gradient-to-r from-primary via-transparent to-primary text-secondary">
-      <Trans>Hecho con</Trans> <span aria-label="heart">💙</span>{" "}
+    <footer className="flex items-center justify-center w-full h-16 text-center bg-gradient-to-r from-primary via-transparent to-primary text-secondary">
+      <Trans>Hecho con</Trans> <span aria-label="heart" className="mx-2">💙</span>{" "}
       <Trans>usando Gatsby, Tailwind CSS y Netlify</Trans>{" "}
       <a
         href="https://github.com/M4ss1ck/gatsby-cv-maker"
-        className="text-accent"
+        className="ml-2 text-accent"
       >
         <Trans>(Código fuente aquí)</Trans>
       </a>
